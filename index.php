1259ede0b86b5339ea55daefea65480c7d946dad
DESKTOP-BFKMUCU\Julio Alfian <julioixc@gmail.com>
2017-09-29 19:22:02 +0700

    implementasi reply message3

--- index.php
+++ index.php
@@ -63,7 +63,7 @@ $app->post('/webhook', function ($request, $response) use ($bot, $pass_signature
                 if($event['message']['type'] == 'text')
                 {
                     // send same message as reply to user
-                    $result = $bot->replyText($event['replyToken'], $event['message']['text']);
+                    //$result = $bot->replyText($event['replyToken'], $event['message']['text']);
 
                     // or we can use replyMessage() instead to send reply message
                     $textMessageBuilder = new TextMessageBuilder('ini pesan balasan');
